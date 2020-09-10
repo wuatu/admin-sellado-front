87765d639adbbabb346646596300e45c11406111
Ignacio Correa <ignaciocorrea21@gmail.com>
2020-09-09 21:59:11 -0300

    Front de usuario en linea (aún no terminado) _ visualizacion de la lista de usuarios según el calibrador y la linea seleccionada funcionando _ Agregar usuario a linea y calibrador funcionando _exportar la tabla a archivo excel funcionando _ Interfas de la busqueda de usuario por rut y fecha con el data picker funcionando

--- src/app/services/usuario-en-linea.service.ts
+++ src/app/services/usuario-en-linea.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioEnLinea } from '../models/usuario-en-linea';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioEnLineaService {
+  API_URL="http://localhost:3000/api"
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  getUsuariosEnLinea(idLinea:string, idCalibrador:string){
+    return this.httpClient.get(`${this.API_URL}/usuarios_en_linea/${idLinea}/${idCalibrador}`);
+    
+  }
+  
+  saveUsuarioEnLinea(usuarioEnLinea:UsuarioEnLinea){
+    return this.httpClient.post(`${this.API_URL}/usuario_en_linea/`,usuarioEnLinea);
+  }
+
 }
